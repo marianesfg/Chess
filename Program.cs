2aca005f7eb6cb5985f7c9d1641725ff2d0d12e1
marianesfg <mariane.sfgodoy@gmail.com>
2020-07-12 17:55:27 -0300

    Começando a colocar peças no tabuleiro

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
-﻿using tabuleiro;
-using System;
+﻿using System;
+
+using tabuleiro;
+using xadrez;
 
 namespace xadrez_console
 {
@@ -9,6 +11,10 @@ namespace xadrez_console
         {
             Posicao p = new Posicao(3, 4);
             Tabuleiro tab = new Tabuleiro(8, 8);
+
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
             Tela.imprimirTabuleiro(tab);
             
